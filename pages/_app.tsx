749dd82683eb7f6e6f9a1bc01b0d02a5539a76ba
uzun0ff <tsvetomir.uzunov@looming.tech>
2024-10-15 14:05:07 +0300

    fix: crossOrigin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,11 +51,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
       <ThemeProvider>
         <Provider store={rootReducer}>
